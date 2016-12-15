ca33bbadfc1940225a89d1a13dd619e9198810bf
Danei Stephenson <daneistephenson@Daneis-MacBook-Air.local>
2016-12-14 20:20:40 -0500

    work in progress

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'FBo16RpaAiz4jKnTt1fTBTpAC',
+  consumer_secret: 'RI9ZvXiTo5T6fpBMqQIBi7t0cyoE34Fdu2MlqOAyoy8LNOItLT',
+  access_token_key: '807584439018647552-sMxHxYlGQ7FjXRUCX5nw44z1dUgo6Cm',
+  access_token_secret: 'me5U78uvFyDgzz6Yb7PjA9UwbWBeIZMxmJ4BZsfwdXjXU',
+}
---
+++ liri.js
@@ -0,0 +1,61 @@
+var key = require('./keys.js');
+// var key = key.twitterKeys
+var twitnpm = require('twitter');
+
+
+
+var client = new twitnpm ({
+       consumer_key: key.twitterKeys.consumer_key,
+       consumer_secret: key.twitterKeys.consumer_secret,
+       access_token_key: key.twitterKeys.access_token_key,
+       access_token_secret: key.twitterKeys.access_token_secret,
+   });
+
+//commands
+var commands = process.argv[2];
+
+if (commands == 'my-tweets') {
+    var params = {screen_name: 'Test_CodingCamp'};
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+      
+      if (!error) {
+           for (var i = 0; i < tweets.length; i++) {
+               console.log(tweets[i].text)
+           }
+        
+      } else{
+          console.log('broken');
+      }
+    });
+}
+
+var request = require('request');
+var name = process.argv[3];
+var nobody = "http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&r=json&tomatoes=true";
+var url = "http://www.omdbapi.com/?t=" + name + "&y=&plot=short&r=json&tomatoes=true";
+request(url, function (error, response, body) {
+ if (!error && response.statusCode == 200) {
+   console.log("Title: " + JSON.parse(body).Title);
+   console.log("Year: " + JSON.parse(body).Plot);
+   console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+   console.log("Country: " + JSON.parse(body).Country);
+   console.log("Plot: " + JSON.parse(body).Plot);
+   console.log("Actors: " + JSON.parse(body).Actors);
+   console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
+   console.log("Rotten Tomatoes URL: " + JSON.parse(body).tomatoURL);
+ }else{
+     request(nobody, function(error, response,body){
+         if (!error && response.statusCode == 200) {
+   console.log("Title: " + JSON.parse(body).Title);
+   console.log("Year: " + JSON.parse(body).Plot);
+   console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+   console.log("Country: " + JSON.parse(body).Country);
+   console.log("Plot: " + JSON.parse(body).Plot);
+   console.log("Actors: " + JSON.parse(body).Actors);
+   console.log("Rotten Tomatoes Rating: " + JSON.parse(body).tomatoRating);
+   console.log("Rotten Tomatoes URL: " + JSON.parse(body).tomatoURL);
+ }
+
+     });
+ };
+});
